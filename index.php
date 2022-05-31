fa37f877338182caac55881d19a5df384cfc09db
Krrish Mittal <krrishmittal15@gmail.com>
2022-05-31 11:37:26 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo "<h2>Connecting....</h2><br/>";
 
-$con = mysqli_connect("krmittawebdbmysql-server.mysql.database.azure.com","nwmsnopjfr.mysql.database.azure.com","233845YOJ353H675$","mysql");
+$con = mysqli_connect("krmittawebdbmysql-server.mysql.database.azure.com","nwmsnopjfr","233845YOJ353H675$","mysql");
 
 // Check connection
 if (mysqli_connect_errno()) {
