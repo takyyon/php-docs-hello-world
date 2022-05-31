3dbe247e4cdde5dfbd7f1c51a5b890a5abafc1dc
Krrish Mittal <krrishmittal15@gmail.com>
2022-05-31 11:33:17 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
-echo "testing connection the db <br/>";
- 
-$mysqli1 = new mysqli("krmittawebdbmysql-server.mysql.database.azure.com", "nwmsnopjfr.mysql.database.azure.com", "233845YOJ353H675$", "sys");
- 
-if ($mysqli1->connect_error) {
-   echo "Not connected, error: " . $mysqli_connection->connect_error;
-}
-else {
-   echo "Connected.";
+echo "<h2>Connecting....</h2><br/>";
+
+$con = mysqli_connect("krmittawebdbmysql-server.mysql.database.azure.com","nwmsnopjfr.mysql.database.azure.com","233845YOJ353H675$","mysql");
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
 }
 ?>
