86f02a05aae2f2c5153f25ab0b635e9d4faf0f7e
Krrish Mittal <krrishmittal15@gmail.com>
2022-05-31 10:58:50 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-
-echo "Hello World!";
+echo "testing connection the db <br/>";
+ 
+$mysqli1 = new mysqli("sumuthwebappdb1-server.mysql.database.azure.com", "sumuth", "iis6!dfu", "mysql");
+ 
+if ($mysqli1->connect_error) {
+   echo "Not connected, error: " . $mysqli_connection->connect_error;
+}
+else {
+   echo "Connected.";
+}
+?>
